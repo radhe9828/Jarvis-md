e9229b66ff1a030ebe252ca7cae594454f99a4f6
radhe9828 <155523143+radhe9828@users.noreply.github.com>
2024-07-19 16:17:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_dc0d_6c73_8723_f460_6731_bc18_55c3_d33b',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_fefb_e608_400e_ea96_1732_08fa_af0d_6cd2',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLER  || '/',
